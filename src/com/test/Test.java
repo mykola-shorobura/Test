3957ba283f4aea6050ab917c59108b07988debee
mshorobura <mshorobura@griddynamics.com>
2018-05-08 16:55:56 +0300

    commit 2

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(" commit 1");
-        System.out.println(" commit 2");
+        System.out.println("commit 1");
+        System.out.println("commit 2");
     }
 }
