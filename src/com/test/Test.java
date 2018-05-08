d95b53023ec37f22c843f89487f4ad50596c579e
mshorobura <mshorobura@griddynamics.com>
2018-05-08 18:45:31 +0300

    text commit

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -4,5 +4,23 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("commit 1");
         System.out.println("commit 2");
+        System.out.println("commit 3");
+        System.out.println("commit 4");
+        System.out.println("commit 5");
+        System.out.println("commit 6");
+        System.out.println("commit 7");
+        System.out.println("commit 8");
+        System.out.println("commit 9");
+        System.out.println("commit 10");
+        System.out.println("commit 11");
+        System.out.println("commit 12");
+        System.out.println("commit 13");
+        System.out.println("commit 14");
+        System.out.println("commit 15");
+        System.out.println("commit 16");
+        System.out.println("commit 17");
+        System.out.println("commit 18");
+        System.out.println("commit 19");
+        System.out.println("commit 20");
     }
 }
