50bf311f91c5e007317603ec28f725da855712c7
mshorobura <mshorobura@griddynamics.com>
2018-05-16 10:18:55 +0300

    delete content commit

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -13,17 +13,5 @@ public class Test {
         System.out.println("commit 9");
         System.out.println("commit 10");
         System.out.println("commit 11");
-        System.out.println("test commit");
-        System.out.println("commit 12");
-        System.out.println("commit 13");
-        System.out.println("commit 14");
-        System.out.println("commit 15");
-        System.out.println("commit 16");
-        System.out.println("commit 17");
-        System.out.println("commit 18");
-        System.out.println("commit 19");
-        System.out.println("commit 20");
-        System.out.println("new_branch");
-        System.out.println("test_new_branch");
     }
 }
